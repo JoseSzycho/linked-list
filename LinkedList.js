dea6731fc4666f6c8069dd3de2187fddf0de7322
Jose Szycho <joseszychowski@gmail.com>
2023-01-27 13:06:28 -0300

    add append method to class)

--- LinkedList.js
+++ LinkedList.js
@@ -1,7 +1,22 @@
+import Node from "./Node.js";
+
 class LinkedList{
     constructor(){
-        this.head= null;
+        this.head = new Node();
     }
+
+    append(value){
+        let pointer = this.head;
+        
+        while(pointer.nextNode != null){
+            pointer = pointer.nextNode;
+        }
+
+        pointer.value = value;
+        pointer.nextNode = new Node();
+    }
+
+
 }
 
 export default LinkedList;
--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 //must create package.json in order to work
 
 import LinkedList from "./LinkedList.js";
-import Node from "./Node.js";
+
+let list = new LinkedList();
+
+list.append(5)
+list.append(4)
+list.append(2)
+list.append(3)
+list.append(1)
+
+console.log(list.show());
